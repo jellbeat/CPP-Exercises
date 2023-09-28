712b461b493bec8bc822778d62f668881724d61e
Jasper Luis Escalona <146302776+jellbeat@users.noreply.github.com>
2023-09-28 10:40:41 +0800

    Add files via upload

---
+++ Activity 8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	string fname;
+	
+	
+	cout<<"Enter Your Name: ";
+	getline(cin,fname);
+	
+	cout<<"The lenght of your name is: "<<fname.size();
+	
+	
+	
+	return 0;
+}
