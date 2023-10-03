378a0de21a15f69229b8b036ee887d7a9267faf9
Jasper Luis Escalona <146302776+jellbeat@users.noreply.github.com>
2023-10-02 19:17:18 -0700

    Add files via upload

---
+++ Activiy 11.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	
+	int grade;
+	
+	cout<<"Enter your grade: ";
+	cin>>grade;
+	
+	
+	if(grade>74){
+		
+		cout<<"Passed\n";
+		cout<<"Well done";
+	}else{
+		
+		cout<<"Failed\n";
+		cout<<"Better luck next time";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
